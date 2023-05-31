dd5d7eb8503b84ebe954ebb95f50090e59b3f97e
Anastasia Irzhyckaya <57043957+anastasiaromanovna33@users.noreply.github.com>
2023-05-31 11:18:58 +0300

    lesson_1

--- Lesson_1/main.js
+++ Lesson_1/main.js
@@ -1,2 +1,72 @@
-ljfkdfl
-dlfkldfkld
+console.log('my name is');
+
+let a = 5;
+let s = 'hello';
+
+console.log(a);
+console.log(s);
+
+//const
+const myConst = 5;
+console.log(myConst);
+
+//typeof
+let name = 'Max';
+console.log(typeof name);
+
+//boolean
+let check = true;
+let check2 = false;
+console.log(5 < 2);
+console.log(5 === 5);
+
+//String
+let name1 = 'Max'
+let surname = 'Popov'
+let houseNum = 55
+
+console.log(name1, surname, houseNum)
+
+let user = name1 + ' ' + surname + ' ' + houseNum;
+console.log(user);
+
+let user2 = `My name is ${name1}, surname: ${surname} and house number is ${houseNum}`
+console.log(user2);
+
+//math
+console.log(5 + 3);
+console.log(5 - 3);
+console.log(5 * 3);
+console.log(5 / 3);
+console.log(5 % 3);
+console.log(14 % 3);
+console.log(5 ** 3);
+console.log(5 > 3);
+console.log(5 < 3);
+console.log(5 >= 3);
+console.log(5 <= 3);
+console.log(5 == 3);
+console.log(5 === 3);
+console.log(5 != 3);
+console.log(5 !== 3);
+console.log('5' == 5);
+console.log('5' === 5);
+console.log(true == 1);
+console.log(true === 1);
+
+console.log(150 + 150 + 'hello');
+
+let asd = null;
+console.log(asd);
+
+let f = 50
+let g = 10
+console.log(f < g);
+if (f<g){
+    console.log('f<g');
+}else{
+    console.log('f>g');
+}
+
+let name3 = prompt()
+console.log(name3);
