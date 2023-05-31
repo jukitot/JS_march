791552d850fc72aa096fb03cd9661007247eec0c
Anastasia Irzhyckaya <57043957+anastasiaromanovna33@users.noreply.github.com>
2023-05-31 19:04:55 +0300

    lesson_2

--- Lesson_2/main.js
+++ Lesson_2/main.js
@@ -97,3 +97,17 @@ if (color === 'green') {
 }else{
     console.log('????');
 }
+
+switch (color) {
+    case "green":
+        console.log('go');
+        break;
+    case "yellow":
+        console.log('wait');
+        break;
+    case "red":
+        console.log('stop');
+        break;
+    default:
+        console.log('????');
+}
