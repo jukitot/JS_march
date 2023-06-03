035f068feee75c71223cd3527b35de2feb296122
Anastasia Irzhyckaya <57043957+anastasiaromanovna33@users.noreply.github.com>
2023-06-03 13:19:29 +0300

    lesson_6

---
+++ Lesson_6/main.js
@@ -0,0 +1,19 @@
+let s1 = 'hello my name is my age is and i';
+console.log(s1[0]);
+let s2 = s1.concat(' are you?');
+console.log(s2);
+
+let upper = s1.toUpperCase();
+console.log(upper);
+
+let lower = upper.toLowerCase();
+console.log(lower);
+
+let starts = s1.startsWith('hello');
+console.log(starts);
+
+let ends = s1.endsWith('dtet');
+console.log(ends);
+
+let substring = s1.substring(4, 10);
+console.log(substring);
