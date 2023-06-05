9e3d5b434fe6d300bc7154fa64c70c99b3b70af6
Anastasia Irzhyckaya <57043957+anastasiaromanovna33@users.noreply.github.com>
2023-06-05 08:41:39 +0300

    lesson_6

--- Lesson_6/main.js
+++ Lesson_6/main.js
@@ -17,3 +17,32 @@ console.log(ends);
 
 let substring = s1.substring(4, 10);
 console.log(substring);
+
+let size = s1.length;
+let substr = s1.substring(size / 2 - 3, size / 2 + 3);
+console.log(substr);
+
+console.log(s1.indexOf('is'));
+
+let indexOfText = s1.indexOf('age');
+console.log(indexOfText);
+let Text = s1.substring(indexOfText, indexOfText + 10);
+console.log(Text);
+
+let data = 'vasya-31, petya-28, sofia-44';
+let dataSplit = data.split(',');
+console.log(dataSplit);
+
+console.log(s1.charAt(10));
+console.log(s1.indexOf('a'));
+
+console.log(s1.lastIndexOf('a'));
+
+console.log(s1.replaceAll('i', '!'));
+
+let phone ='+38 (067)-338-12-12';
+console.log(phone
+    .replaceAll('+38', '')
+    .replaceAll('(', '')
+    .replaceAll(')', '')
+    .replaceAll('-', ''));
