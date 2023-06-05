771a6a27c1b4f05dd6bd6e3c6c8853d6f9d9b256
Anastasia Irzhyckaya <57043957+anastasiaromanovna33@users.noreply.github.com>
2023-06-05 20:21:42 +0300

    lesson_7

--- Lesson_7/main.js
+++ Lesson_7/main.js
@@ -57,9 +57,10 @@ console.log(user?.wife?.name);
 let user1 = {
     name: 'kokos',
     age: 123,
-    foobar() {
-
-    },
+    // foobar() {
+    //
+    // },
+    wife: {}
 }
 
 let user2 = user1;
@@ -67,3 +68,56 @@ let s = JSON.stringify(user1);
 console.log(s);
 console.log(typeof s);
 
+let obj = JSON.parse(s);
+console.log(obj);
+console.log(obj === user1);
+
+let assign = Object.assign({}, user1);
+console.log(assign);
+console.log(assign === user1);
+
+console.log(assign.wife === user1.wife);
+
+// let clone = structuredClone(user1);
+// console.log(clone === user);
+
+
+// let name = 'kokos';
+// let age = 123
+// let user11 = {
+//     name,
+//     age,
+//     foo() {
+//
+//     }
+// }
+//
+// console.log(user11);
+
+let user12 = {
+    name: 'kokos',
+    age: 123,
+    foobar() {
+
+    },
+    wife: {}
+}
+
+let {name, age, foobar, wife} = user12;
+console.log(name);
+console.log(age);
+console.log(wife);
+console.log(foobar);
+
+let user22 = {
+    name: 'kok',
+    age: 111,
+    foobar() {
+
+    },
+    wife: {}
+}
+
+let {age: age1, foobar: foobar1, name: name1, wife: wife1} = user22;
+console.log(age1);
+console.log(name1);
