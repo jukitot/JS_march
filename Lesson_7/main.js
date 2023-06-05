7d28eee1f5595f6581d60daa0defb7adec7fa4c1
Anastasia Irzhyckaya <57043957+anastasiaromanovna33@users.noreply.github.com>
2023-06-05 18:52:10 +0300

    lesson_7

---
+++ Lesson_7/main.js
@@ -0,0 +1,13 @@
+let date = new Date();
+console.log(date);
+console.log(12443);
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDay());
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getMilliseconds());
+console.log(date.getTime());
+
+console.log(new Date(1685980116421));
+console.log(new Date(1981, 0, 31, 12, 30, 45));
