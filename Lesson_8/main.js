e77af87411c9fcc3043de94e240a1c71e585e6e7
Anastasia Irzhyckaya <57043957+anastasiaromanovna33@users.noreply.github.com>
2023-06-07 17:51:46 +0300

    lesson_8

--- Lesson_8/main.js
+++ Lesson_8/main.js
@@ -1,5 +1,51 @@
 console.log('lesson_8');
 
-function User() {
-
+function User(name, age) {
+    this.name = name;
+    this.age = age;
 }
+
+let u1 = new User();
+console.log(u1);
+
+console.log(new User());
+
+let u2 = new User('kakos', 123);
+console.log(u2);
+
+let simpleUsers = [
+    new User('sdd', 123),
+    new User('woei', 45)
+];
+
+console.log(simpleUsers);
+
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true},
+];
+
+function Customer(name, age, id) {
+    this.name = name;
+    this.age = age;
+    this.id = id;
+}
+
+
+
+let customer = users.map(value => new Customer(value.name, value.age, new Date().getTime()));
+console.log(customer);
+
+let array = new Array(10);
+
+let date = new Date();
+console.log(date);
