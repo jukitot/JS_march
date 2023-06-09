5113960e4f399dd65aa50df241557f005f5901bb
Anastasia Irzhyckaya <57043957+anastasiaromanovna33@users.noreply.github.com>
2023-06-09 15:08:25 +0300

    lesson_10

--- Lesson_10/main.js
+++ Lesson_10/main.js
@@ -1,1 +1,20 @@
 console.log('local storage');
+
+// function explorer(htmlElement){
+//     console.log(htmlElement);
+//     let children = htmlElement.children;
+//     if(children.length !== 0) {
+//         for (const child of children) {
+//             explorer(child);
+//         }
+//     }
+// }
+//
+// explorer(document.body);
+console.log(document.forms[0]);
+let form1 = document.forms.form1;
+console.log(form1);
+
+console.log(form1.firstname);
+console.log(form1.email);
+console.log(form1.firstname.value);
