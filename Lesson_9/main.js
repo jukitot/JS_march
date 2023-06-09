40f15c9f43e7b484b23ad6a06e42a063e6c5b86f
Anastasia Irzhyckaya <57043957+anastasiaromanovna33@users.noreply.github.com>
2023-06-09 11:22:59 +0300

    lesson_9

--- Lesson_9/main.js
+++ Lesson_9/main.js
@@ -1,3 +1,27 @@
 console.log('dom');
 console.log(document);
 console.log(document.children[0]);
+
+let htmlChild = document.children[0];
+console.log(htmlChild);
+console.log(htmlChild.children);
+
+let headChild = htmlChild.children[0];
+console.log(headChild);
+let headElement = document.head;
+console.log(headElement);
+let titles = document.head.getElementsByTagName('title');
+
+console.log(titles);
+let title = titles[0];
+console.log(title);
+console.log(title.innerHTML);
+
+let bodyChild = htmlChild.children[1];
+console.log(bodyChild);
+console.log(document.body);
+console.log(document.body.innerHTML);
+
+let metas = document.head.getElementsByTagName('meta');
+console.log(metas);
+
